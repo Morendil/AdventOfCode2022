636955f561fe35f2bac6b0950756be37e8ead4e9
Laurent Bossavit <laurent.git@bossavit.com>
2022-12-06 08:11:28 +0100

    Day 6 part 2

--- Day06.hs
+++ Day06.hs
@@ -4,8 +4,17 @@ windows :: Int -> [a] -> [[a]]
 windows n = transpose . take n . tails
 
 part1 :: String -> Int
-part1 = (+4) . length . takeWhile (\s -> (length $ nub s) /= 4) . windows 4
+part1 = solve 4
+
+part2 :: String -> Int
+part2 = solve 14
+
+solve :: Int -> String -> Int
+solve n = (+n) . length . takeWhile (\s -> (length $ nub s) /= n) . windows n
+
 
 main = do
     signal <- lines <$> readFile "day06.txt"
     mapM_ (print . part1) signal
+    print "---"
+    mapM_ (print . part2) signal
