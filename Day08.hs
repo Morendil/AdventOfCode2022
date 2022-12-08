747fbe4b71a3b06e4a3c67c73f052ad9a0e960e7
Laurent Bossavit <laurent.git@bossavit.com>
2022-12-08 09:07:52 +0100

    Day 8 part 2

--- Day08.hs
+++ Day08.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List.HT (takeUntil)
 
 part1 :: [[Int]] -> Int
 part1 grid = length [() | x <- [0..size-1], y <- [0..size-1], visible x y]
@@ -10,6 +11,17 @@ part1 grid = length [() | x <- [0..size-1], y <- [0..size-1], visible x y]
                         shorter [grid !! yy !! x | yy <- [y..size-1]]
           shorter (x:xs) = not $ any (>= x) xs
 
+part2 :: [[Int]] -> Int
+part2 grid = maximum [score x y | x <- [0..size-1], y <- [0..size-1]]
+    where size = length grid
+          score x y | x == 0 || y == 0 || x == size-1 || y == size -1 = 0
+          score x y = dist [grid !! y !! xx | xx <- [x,x-1..0]] *
+                      dist [grid !! y !! xx | xx <- [x..size-1]] *
+                      dist [grid !! yy !! x | yy <- [y,y-1..0]] *
+                      dist [grid !! yy !! x | yy <- [y..size-1]]
+          dist (x:xs) = length $ takeUntil (>= x) xs
+
 main = do
     grid <- map (map digitToInt) . lines <$> readFile "day08.txt"
     print $ part1 grid
+    print $ part2 grid
