c78a2c27376f3f6b668a963b594cda14ce0553d6
Laurent Bossavit <laurent.git@bossavit.com>
2022-12-10 09:47:08 +0100

    Day 10 part 2

--- Day10.hs
+++ Day10.hs
@@ -4,9 +4,17 @@ unfold = reverse . foldl expand [1]
           expand (last:rest) addx = (read $ drop 5 addx)+last:last:last:rest
 
 part1 :: [String] -> Int
-part1 instructions = sum $ map (\n -> n * (xvalues !! (n-1))) [20, 60, 100, 140, 180, 220]
+part1 instructions = sum $ map (\n -> signal !! (n-1)) [20, 60, 100, 140, 180, 220]
     where xvalues = unfold instructions
+          signal = zipWith (*) xvalues [1..]
+
+part2 :: [String] -> [String]
+part2 instructions = [[pixel x y | x <- [0..39]] | y <- [0..5]]
+    where xvalues = unfold instructions
+          sprite x y = xvalues !! ((y*40)+x)
+          pixel x y = if x == sprite x y - 1 || x == sprite x y || x == sprite x y + 1 then '#' else '.'
 
 main = do
     instructions <- lines <$> readFile "day10.txt"
     print $ part1 instructions
+    mapM_ putStrLn $ part2 instructions
