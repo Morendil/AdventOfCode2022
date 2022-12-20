6cfd9c68ddfcca5eb74693f258bdb61fc6ebf85c
Laurent BOSSAVIT <216452+Morendil@users.noreply.github.com>
2022-12-20 15:00:48 +0100

    Day 20 part 1

--- Day20.hs
+++ Day20.hs
@@ -7,9 +7,12 @@ type Encrypted = M.Map Int (Int,(Int, Int))
 part1 :: [Int] -> Int
 part1 numbers = flattened !! (1000`mod`n) + flattened !! (2000`mod`n) + flattened !! (3000`mod`n)
     where encrypted = encFromList numbers
-          mixed = foldl (flip move) encrypted [1..n]
-          flattened = encToList mixed
+          flattened = encToList $ mix encrypted
           n = length numbers
+
+mix :: Encrypted -> Encrypted
+mix encrypted = foldl (flip move) encrypted [1..n]
+    where n = M.size encrypted
 
 encFromList :: [Int] -> Encrypted
 encFromList numbers = M.fromList $ zip [1..n] $ zip numbers $ zip (drop (n-1) (cycle [1..n])) (tail (cycle [1..n]))
@@ -24,19 +27,21 @@ parse :: String -> Int
 parse = read
 
 move :: Int -> Encrypted -> Encrypted
-move n encrypted = if value == 0 then encrypted else makeMove encrypted
+move n encrypted = if offset == 0 then encrypted else makeMove encrypted
     where start@(value,(leftOfStart, rightOfStart)) = fromJust $ M.lookup n encrypted
           right (_,(_,r)) = fromJust $ M.lookup r encrypted
           left (_,(l,_)) = fromJust $ M.lookup l encrypted
           step = if value < 0 then left else right
           swap = if value < 0 then reverse else id
-          [(_,(_,newRight)), (_,(newLeft,_))] = swap $ drop (abs value) $ take (2+(abs value)) $ iterate step start
+          [(_,(_,newRight)), (_,(newLeft,_))] = swap $ drop offset $ take (2+offset) $ iterate step start
+          offset = abs value `mod` (len-1)
           makeMove = unlinkLeft . unlinkRight . linkLeft . linkRight . placeCell
           unlinkLeft = M.adjust (second (second $ const rightOfStart)) leftOfStart
           unlinkRight = M.adjust (second (first $ const leftOfStart)) rightOfStart
           linkRight = M.adjust (second (first $ const n)) newRight
           linkLeft = M.adjust (second (second $ const n)) newLeft
           placeCell = M.adjust (second $ const (newLeft, newRight)) n
+          len = M.size encrypted
 
 main = do
     numbers <- map parse . lines <$> readFile "day20.txt"
